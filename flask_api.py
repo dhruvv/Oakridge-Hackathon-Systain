2f5dbd9aaebd903ffdb7cfca01471a9a1c235713
dhruvv <dhruva.venkataraman@gmail.com>
2018-01-20 12:18:14 +0530

    Python Server now supports the Entire JSON

--- flask_api.py
+++ flask_api.py
@@ -1,13 +1,15 @@
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-data = {"data":0}
+data = {"turbidity":0, "lat":0, "long":0, "temperature":0}
 
 @app.route('/put')
 def put():
-    data['data'] = request.form['data']
+    data['turbidity'] = request.form['turbidity']
+    data['lat'] = request.form['lat']
+    data['long'] = request.form['long']
+    data['temperature'] = request.form['temperature']
+    return jsonify(data)
 @app.route('/')
 def get():
     return jsonify(data)
-
-    
