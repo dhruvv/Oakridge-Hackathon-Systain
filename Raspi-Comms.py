95d41ae167ed9221799eef390cb81b9743b71ac1
dhruvv <dhruva.venkataraman@gmail.com>
2018-01-20 19:58:05 +0530

    Update Raspi-Comms.py

--- Raspi-Comms.py
+++ Raspi-Comms.py
@@ -4,5 +4,9 @@ import requests
 arduino = serial.Serial("/dev/ttyACM0", 9600)
 while True :
   msg = arduino.readline()
-  msg = str(msg)
-  requests.put("https://mralpaca.pythonanywhere.com/put", data=msg)
+  msg = str(msg).split("\\")[0].split("\'")[1]
+  gps_lat = "67N"
+  gpa_long = "82E"
+  data = {"data":a, "lat":gps_lat, "long":gps_long, "temperature":"0"}
+  x = requests.put("https://oakhack.pythonanywhere.com/put", json=data)
+  print(x.text)
