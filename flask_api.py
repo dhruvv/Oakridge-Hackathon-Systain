feb98b5dc385383a5b999440690678e46ae3e748
dhruvv <dhruva.venkataraman@gmail.com>
2018-01-21 01:46:14 +0530

    Add the POST method to support the Arduino Wifi methods

--- flask_api.py
+++ flask_api.py
@@ -1,15 +1,19 @@
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-data = {"turbidity":0, "lat":0, "long":0, "temperature":0}
+data = {"turbidity":"0", "lat":0, "long":0, "temperature":0}
 
-@app.route('/put')
+@app.route('/put', methods=["PUT"])
 def put():
-    data['turbidity'] = request.form['turbidity']
-    data['lat'] = request.form['lat']
-    data['long'] = request.form['long']
-    data['temperature'] = request.form['temperature']
+    global data
+    data = request.get_json(force=True)
     return jsonify(data)
 @app.route('/')
 def get():
+    return jsonify(data)
+
+@app.route('/post', methods=["POST"])
+def post():
+    global data
+    data['turbidity'] = request.get_json(force=True)
     return jsonify(data)
